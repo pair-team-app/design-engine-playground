7dd4efdbbd21be08c9353ed4ac2fb97c5c04c4fe
mtholcombe <matt@designengine.ai>
2019-09-04 07:33:18 -0700

    changing url

--- src/main.js
+++ src/main.js
@@ -285,10 +285,10 @@ export async function puppet() {
 		await browser.close();
 		server.close();
 
-		console.log('%s Playground created! %s', chalk.green.bold('DONE'), chalk.blue.bold(`http://playground.designengine.ai/${playground.id}`));
+		console.log('%s Playground created! %s', chalk.green.bold('DONE'), chalk.blue.bold(`http://playground.designengine.ai/spectrum-adobe-${playground.id}`));
 		if (!openedPlayground) {
 			await writeCache('playground_open', true);
-			open(`http://playground.designengine.ai/${playground.id}`);
+			open(`http://playground.designengine.ai/spectrum-adobe-${playground.id}`);
 //			open(`http://playground.designengine.ai/${55}`);
 		}
 	});
