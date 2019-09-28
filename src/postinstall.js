d6a744d2d76c180beee42b7ab13627c7df8972a6
mtholcombe <matt@designengine.ai>
2019-09-28 13:46:44 -0700

    adding post install

--- src/postinstall.js
+++ src/postinstall.js
@@ -1,2 +1,55 @@
+#!/usr/bin/env node
 
 console.log('Post-install script HERE!');
+
+import chalk from "chalk/types/index";
+import fs from 'fs';
+import path from 'path';
+import { promisify } from 'util';
+
+
+const access = promisify(fs.access);
+const PACKAGE_PATH = path.join(process.cwd(), 'package.json');
+
+
+function normalize(data) {
+	let scripts = data.scripts || {};
+
+	if (typeof scripts === 'string' || scripts instanceof String) {
+		const v = scripts;
+		scripts = {};
+		scripts['postbuild'] = v;
+	}
+
+	return (scripts);
+}
+
+function addPostbuild(data) {
+	let scripts = normalize(data);
+	scripts['postbuild'] = 'design-engine-playground';
+	data.scripts = scripts;
+
+	return (data);
+}
+
+function prettyPrint(data) {
+	return (JSON.stringify(data, null, 4));
+}
+
+function savePackage(data) {
+	return (fs.writeFileAsync(PACKAGE_PATH, data));
+}
+
+
+(async function() {
+	try {
+		await access(PACKAGE_PATH, fs.constants.R_OK);
+
+	} catch (e) {
+		console.log('%s Couldn\'t find package.json! %s', chalk.red.bold('ERROR'), e);
+		process.exit(1);
+	}
+
+	fs.readFileAsync(PACKAGE_PATH).then(JSON.parse).then(addPostbuild).then(prettyPrint).then(savePackage).catch(console.log);
+	console.log('%s Successfully installed design-engine-playground!', chalk.green.bold('INFO'))
+})();
