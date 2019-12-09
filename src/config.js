b8215a1cfd81400706c27de0cddafa608b41fcaa
mtholcombe <matt@designengine.ai>
2019-12-08 19:29:39 -0800

    switching creation api calls off

--- src/config.js
+++ src/config.js
@@ -2,8 +2,8 @@
 'use strict';
 
 
-export const MAKE_PLAYGROUND = true;
-//export const MAKE_PLAYGROUND = false;
-
-export const SEND_ELEMENTS = true;
-//export const SEND_ELEMENTS = false;
+//export const MAKE_PLAYGROUND = true;
+export const MAKE_PLAYGROUND = false;
+//
+//export const SEND_ELEMENTS = true;
+export const SEND_ELEMENTS = false;
